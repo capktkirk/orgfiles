5f53f72d560972855b4a1c4d2f30828b0c3a8993
capktkirk <ktkirk@mail.csuchico.edu>
2020-07-13 11:15:52 -0700

    Adjusting testing of hash tables.

--- go/data_structs/main.go
+++ go/data_structs/main.go
@@ -40,7 +40,35 @@ func main() {
 	//Stack is the same as Queue essentially.
 
 	//5th Hash Tables
+	//When using maps in Golang, you need to have the variable, and then "make" the map available.
+	hashAmt := 2056
+	var hashT map[int]string
+	hashT = make(map[int]string)
+	hashIdx := HashSlot("Dogwood", hashAmt)
+	hashT[hashIdx] = "Dogwood"
+	fmt.Println(hashT[HashSlot("Dogwood", hashAmt)])
 
+	//Can we auto-assign a map?
+	hashT2 := make(map[int]string)
+	hashT2[HashSlot("Dilbert", hashAmt)] = "Dilbert"
+	fmt.Println(hashT2[HashSlot("Dilbert", hashAmt)])
+	//Yes we can.
+	//We could make this more thorough by having each map contain a map instead of a string, then doing another hash to get to that for collisions. The chances of a double hash being the same is very unlikely.
+	//We can create an anonymous function in the declaration of HashTable, and then fill it in with a specific one when we create the new HashTable. We could actually do this infinitely with changing our map to contain a hashtable instead of a string.
+	ht := HashTable{
+		hash: make(map[int]string),
+		idx:  2056,
+		hSlot: func(s string, x int) int {
+			tot := 1
+			for i := 0; i < len(s); i++ {
+				tot = int(s[i]) * tot
+			}
+			tot = tot % x
+			return tot
+		},
+	}
+	ht.hash[ht.hSlot("Dany", ht.idx)] = "Dany"
+	fmt.Println(ht.hash[ht.hSlot("Dany", ht.idx)])
 	//6th Trees
 
 	//7th Maybe Heaps
